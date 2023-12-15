754267654ab281151a32f532afd181d3bd2c73ef
mizanurrrahaman <rodking1414@gmail.com>
2023-12-15 08:43:28 -0800

    section add

---
+++ src/sections/journey/Journey..jsx
@@ -0,0 +1,14 @@
+<section>
+<div className="container">
+   <div className="journey-wrapper">
+      <div className="journer-title">
+          <h2>Our Journey</h2>
+           <p>Our story is one of continuous growth and evolution. We started as a small team with big dreams, determined to create a real estate platform that transcended the ordinary. Over the years, we've expanded our reach, forged valuable partnerships, and gained the trust of countless clients.</p>
+
+      </div>
+      <div>
+        
+      </div>
+   </div>
+</div>
+</section>
