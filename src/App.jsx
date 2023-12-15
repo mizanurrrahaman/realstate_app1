952796386fa6abb6adae3d410c19c5cf1a935c62
Masrafi Mondol <masrafigd20@gmail.com>
2023-12-15 21:25:38 +0600

    headline image

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Home from './pages/Home';
 import Contact from './pages/Contact';
 import AboutUs from './pages/AboutUs';
 import Properties from './pages/Properties';
+import PropertyDetails from './pages/PropertyDetails';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/aboutus' element={<AboutUs/>}/>
       <Route path='/properties' element={<Properties/>}/>
+      <Route path='/propertydetails' element={<PropertyDetails/>}/>
     </Route>
   )
 );
