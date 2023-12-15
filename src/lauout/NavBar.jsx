d2342980936ea3ea086f40dcbec0eebed2c20238
Masrafi Mondol <masrafigd20@gmail.com>
2023-12-15 21:05:44 +0600

    react router dom

---
+++ src/lauout/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <div className="logo"></div>
+        <div className="menu">
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+    </>
+  )
+}
+
+export default NavBar
